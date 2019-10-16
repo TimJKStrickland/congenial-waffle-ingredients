4d85b07dec2bed4f9a17f3f4012313bfa88e1962
timjkstrickland <timjkstrickland@gmail.com>
2019-10-16 00:53:41 -0700

    First draft of calculator

--- client/src/components/libraries/Calculator.js
+++ client/src/components/libraries/Calculator.js
@@ -1,88 +1,133 @@
 import React, { Component } from 'react'
 import { Button } from 'semantic-ui-react'
 
+
+const operators = {
+  'add': (first, second) => first + second,
+  'subtract': (first, second) => first - second,
+  'divide': (first, second) => first / second,
+  'multiply': (first, second) => first * second,
+  'equals': (first, second) => second
+}
 export default class Calculator extends Component {
   constructor(props){
     super(props);
     this.state = {
-      formula: "",
-      endValue: 0
-    }
-  }
-  handleClick(button){
-    const btn = button.currentTarget.innerHTML;
-    const display = document.getElementById('display');
-    var numReg = /\d+/;
-    if(numReg.test(btn)){
-      if (display.innerHTML !== "0"){
-        display.innerHTML += btn
-        this.setState({
-          formula: display.innerHTML
-        })
-      } else {
-        display.innerHTML = btn
-      }
-      console.log("TCL: Calculator -> handleClick -> display", display)
+      display: "0",
+      firstOperand: null,
+      secondOperand: null,
+      waitingForSecondOperand: false,
+      nextOperator: null,
+      excludeMinus: false
     }
   }
 
-  clearClick(){
-    const display = document.getElementById('display');
-    display.innerHTML = "0";
-  }
-
-  add(btn) {
-    const display = document.getElementById('display');
-    const displayValue = Number(display.innerHTML);
-    const btnValue = btn.currentTarget.innerHTML;
-    console.log("TCL: Calculator -> add -> display", display)
-    console.log("TCL: Calculator -> add -> displayValue", displayValue)
-    display.innerHTML += btnValue
+  clearClick() {
     this.setState({
-      formula: display
+      display: "0",
+      firstOperand: null,
+      secondOperand: null,
+      waitingForSecondOperand: false,
+      nextOperator: null,
+      excludeMinus: false
     })
   }
-  sub(btn) {
-    const display = document.getElementById('display');
-    const displayValue = Number(display.innerHTML);
-    console.log("TCL: Calculator -> add -> display", display)
-    console.log("TCL: Calculator -> add -> displayValue", displayValue)
+
+  handleDigit(button){
+    const { waitingForSecondOperand, display, excludeMinus } = this.state;
+    const btn = button.currentTarget.innerHTML;
+    const currentValue = document.getElementById('display').innerHTML
+    if (btn == "." && currentValue.includes('.')){
+      return
+    }
+    if(excludeMinus){
+      this.setState({
+        display: "-" + btn,
+        excludeMinus: false,
+        waitingForSecondOperand: false
+      })
+      return
+    }
+    if(waitingForSecondOperand) {
+      this.setState({
+          waitingForSecondOperand: false,
+          display: btn
+      });
+    } else {
+      if (display !== "0"){
+        this.setState((prevState) => {
+          return {
+            display: prevState.display + btn
+          }
+        })
+      } else {
+        this.setState({
+          display: btn
+        })
+      }
+    }
   }
-  div(btn) {
-    const display = document.getElementById('display');
-    const displayValue = Number(display.innerHTML);
-    console.log("TCL: Calculator -> add -> display", display)
-    console.log("TCL: Calculator -> add -> displayValue", displayValue)
-  }
-  mult(btn) {
-    const display = document.getElementById('display');
-    const displayValue = Number(display.innerHTML);
-    console.log("TCL: Calculator -> add -> display", display)
-    console.log("TCL: Calculator -> add -> displayValue", displayValue)
+
+  handleOperator(clickedOperator) {
+    const nextOp = clickedOperator.currentTarget.id;
+    const { display, firstOperand, waitingForSecondOperand, nextOperator } = this.state;
+    if(nextOperator && waitingForSecondOperand) {
+      if(nextOp == "subtract"){
+        this.setState((prevState) => {
+          return {
+            nextOperator: prevState.nextOperator,
+            excludeMinus: true
+          }
+        });
+        return
+      } else {
+        this.setState({
+          nextOperator: nextOp,
+          excludeMinus: false
+        })
+      }
+    } else if(firstOperand == null) {
+      this.setState({
+        firstOperand: Number(display)
+      })
+    } else if(nextOperator) {
+      const currentValue = firstOperand || 0;
+      console.log("TCL: Calculator -> handleOperator -> currentValue", currentValue)
+      const result = operators[nextOperator](currentValue, Number(display));
+      console.log("TCL: Calculator -> handleOperator -> result", result)
+      this.setState({
+        display: result,
+        firstOperand: result,
+      });
+    }
+    this.setState({
+      waitingForSecondOperand: true,
+      nextOperator: nextOp
+    });
   }
 
   render() {
-    const { endValue, formula } = this.state;
+    const { display } = this.state;
     return (
       <div>
-        <Button onClick={this.handleClick.bind(this)} id="zero">0</Button>
-        <Button onClick={this.handleClick.bind(this)} id="one">1</Button>
-        <Button onClick={this.handleClick.bind(this)} id="two">2</Button>
-        <Button onClick={this.handleClick.bind(this)} id="three">3</Button>
-        <Button onClick={this.handleClick.bind(this)} id="four">4</Button>
-        <Button onClick={this.handleClick.bind(this)} id="five">5</Button>
-        <Button onClick={this.handleClick.bind(this)} id="six">6</Button>
-        <Button onClick={this.handleClick.bind(this)} id="seven">7</Button>
-        <Button onClick={this.handleClick.bind(this)} id="eight">8</Button>
-        <Button onClick={this.handleClick.bind(this)} id="nine">9</Button>
-        <Button onClick={this.add.bind(this)} id="add">+</Button>
-        <Button onClick={this.handleClick.bind(this)} id="subtract">-</Button>
-        <Button onClick={this.handleClick.bind(this)} id="multiply">*</Button>
-        <Button onClick={this.handleClick.bind(this)} id="divide">/</Button>
-        <Button onClick={this.handleClick.bind(this)} id="decimal">.</Button>
-        <Button onClick={this.handleClick.bind(this)} id="equals">=</Button>
-        <Button onClick={this.clearClick.bind(this)} id="clear">C</Button>
-        <div onClick={this.handleClick.bind(this)} id="display">{endValue}</div>
+        <Button onClick={this.handleDigit.bind(this)} id="zero">0</Button>
+        <Button onClick={this.handleDigit.bind(this)} id="one">1</Button>
+        <Button onClick={this.handleDigit.bind(this)} id="two">2</Button>
+        <Button onClick={this.handleDigit.bind(this)} id="three">3</Button>
+        <Button onClick={this.handleDigit.bind(this)} id="four">4</Button>
+        <Button onClick={this.handleDigit.bind(this)} id="five">5</Button>
+        <Button onClick={this.handleDigit.bind(this)} id="six">6</Button>
+        <Button onClick={this.handleDigit.bind(this)} id="seven">7</Button>
+        <Button onClick={this.handleDigit.bind(this)} id="eight">8</Button>
+        <Button onClick={this.handleDigit.bind(this)} id="nine">9</Button>
+        <Button onClick={this.handleOperator.bind(this)} id="add">+</Button>
+        <Button onClick={this.handleOperator.bind(this)} id="subtract">-</Button>
+        <Button onClick={this.handleOperator.bind(this)} id="multiply">&times;</Button>
+        <Button onClick={this.handleOperator.bind(this)} id="divide">&divide;</Button>
+        <Button onClick={this.handleDigit.bind(this)} id="decimal">.</Button>
+        <Button onClick={this.handleOperator.bind(this)} id="equals">=</Button>
+        <Button onClick={this.clearClick.bind(this)} id="clear">AC</Button>
+        <div id="display">{ display }</div>
       </div>
     )
   }
