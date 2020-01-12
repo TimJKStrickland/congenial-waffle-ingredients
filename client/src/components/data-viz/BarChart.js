eaaa1d96fb34beb8b6b025f8ac8aa5edf1bd0e69
timjkstrickland <timjkstrickland@gmail.com>
2020-01-11 16:01:07 -0800

    Updates the barchart

--- client/src/components/data-viz/BarChart.js
+++ client/src/components/data-viz/BarChart.js
@@ -1,10 +1,20 @@
 import React, { Component } from 'react'
 
+
+
 export default class BarChart extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+       title : 'title'
+    }
+  }
+  
   render() {
+    const { title } = this.state;
     return (
       <div>
-        
+        <div id="title">{title}</div>
       </div>
     )
   }
